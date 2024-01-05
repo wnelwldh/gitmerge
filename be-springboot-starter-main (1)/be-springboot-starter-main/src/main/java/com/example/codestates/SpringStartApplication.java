a70bb3d0ba828f6e6d4bc7d2a7b13db159b6da8a
Seong Mi Lee <wnelwldh@naver.com>
2024-01-05 15:03:20 +0900

    :art: "프로젝트 폴더파일 올리기"

---
+++ be-springboot-starter-main (1)/be-springboot-starter-main/src/main/java/com/example/codestates/SpringStartApplication.java
@@ -0,0 +1,11 @@
+package com.example.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStartApplication.class, args);
+  }
+}
